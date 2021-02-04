fa5b0c395a5b51b83f79ae9577ecf694a4b555ec
HaeJin Lee <seareale@gmail.com>
2021-02-04 15:02:34 +0900

    Update run_nia_demo.sh

--- run_nia_demo.sh
+++ run_nia_demo.sh
@@ -6,13 +6,13 @@ PATH_NAME=$(pwd)
 count=1
 while [ 1 ]
 	do
-		pid=`ps -ef | grep "nia-web-demo/app.py" | grep -v 'grep' | awk '{print $2}'`
+		pid=`ps -ef | grep "/run_nia_demo.py" | grep -v 'grep' | awk '{print $2}'`
 		if [ -z $pid ] ; then
-			echo "log) NO NIA_"$count >> PATH_NAME'/nia-web-demo/log_NIA.txt'
+			echo "log) NO NIA_"$count >> $PATH_NAME'/log_NIA.txt'
 			if [ $count -gt 3 ] ; then
-				echo "log) NIA DEMO START" >> PATH_NAME'/nia-web-demo/log_NIA.txt'
+				echo "log) NIA DEMO START" >> $PATH_NAME'/log_NIA.txt'
 				count=0
-				nohup python app.py &>> PATH_NAME'/nia-web-demo/log_NIA.txt' &
+				nohup python app.py &>> $PATH_NAME'/log_NIA.txt' &
 			fi
 		fi
 		count=$((count+1))
